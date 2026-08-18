76318045d0a818bd24fe6308c50f0a5c90ee3df3
gitbrert <cuglievan@etik.com>
2026-08-18 16:09:10 +0200

    Use vanilla enchanting GUI texture for Favour screen

--- src/main/java/com/taroflavoured/FavourScreen.java
+++ src/main/java/com/taroflavoured/FavourScreen.java
@@ -1,17 +1,13 @@
 package com.taroflavoured;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class FavourScreen extends AbstractContainerScreen<FavourMenu> {
-    private static final ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(
-            TaroFlavoured.MOD_ID, "textures/gui/container/favour_enchanting.png");
-    private static final ResourceLocation VANILLA_SLOTS = ResourceLocation.withDefaultNamespace(
+    private static final ResourceLocation VANILLA_GUI = ResourceLocation.withDefaultNamespace(
             "textures/gui/container/enchanting_table.png");
 
     public FavourScreen(FavourMenu menu, Inventory inventory, Component title) {
@@ -22,9 +18,12 @@ public class FavourScreen extends AbstractContainerScreen<FavourMenu> {
 
     @Override
     protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        guiGraphics.blit(BACKGROUND, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+        guiGraphics.blit(VANILLA_GUI, leftPos, topPos, 0, 0, imageWidth, imageHeight);
 
+        // Reuse the vanilla enchanting-table panel and remold its upper area into an anvil-style layout.
+        guiGraphics.fill(leftPos + 5, topPos + 7, leftPos + 171, topPos + 82, 0xFFC6C6C6);
+
+        drawArrow(guiGraphics, leftPos + 66, topPos + 30);
         drawVanillaSlot(guiGraphics, 18, 22);
         drawVanillaSlot(guiGraphics, 137, 22);
 
@@ -35,7 +34,19 @@ public class FavourScreen extends AbstractContainerScreen<FavourMenu> {
     }
 
     private void drawVanillaSlot(GuiGraphics guiGraphics, int x, int y) {
-        guiGraphics.blit(VANILLA_SLOTS, leftPos + x, topPos + y, 14, 46, 18, 18);
+        guiGraphics.blit(VANILLA_GUI, leftPos + x, topPos + y, 14, 46, 18, 18);
+    }
+
+    private void drawArrow(GuiGraphics guiGraphics, int x, int y) {
+        int dark = 0xFF4A4A4A;
+        int light = 0xFFBEBEBE;
+
+        guiGraphics.fill(x, y, x + 42, y + 2, light);
+        guiGraphics.fill(x, y + 2, x + 42, y + 4, dark);
+
+        for (int i = 0; i < 7; i++) {
+            guiGraphics.fill(x + 42 - i, y + 1 - i, x + 44 - i, y + 3 + i, dark);
+        }
     }
 
     @Override

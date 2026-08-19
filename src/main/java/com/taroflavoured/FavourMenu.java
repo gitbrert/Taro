d36163aa3c10e7c136bdba40648e10be54fb5368
gitbrert <cuglievan@etik.com>
2026-08-19 19:12:06 +0200

    Align book slots with custom GUI texture

--- src/main/java/com/taroflavoured/FavourMenu.java
+++ src/main/java/com/taroflavoured/FavourMenu.java
@@ -49,12 +49,12 @@ public class FavourMenu extends AbstractContainerMenu {
         checkContainerSize(container, CUSTOM_SLOT_COUNT);
         container.startOpen(playerInventory.player);
 
-        // Match the two book positions in the custom GUI texture.
-        addSlot(new BookInputSlot(container, INPUT_SLOT, 14, 44));
+        // Move the book slots one pixel down and to the right to align with the custom GUI texture.
+        addSlot(new BookInputSlot(container, INPUT_SLOT, 15, 45));
         for (int i = 0; i < 5; i++) {
             addSlot(new IngredientSlot(this, container, INGREDIENT_START + i, 66 + i * 20, 35, i));
         }
-        addSlot(new OutputSlot(container, OUTPUT_SLOT, 34, 44));
+        addSlot(new OutputSlot(container, OUTPUT_SLOT, 35, 45));
 
         addPlayerInventory(playerInventory);
         addDataSlots(data);

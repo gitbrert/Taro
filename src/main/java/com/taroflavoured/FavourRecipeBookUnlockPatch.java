d311ef49d89307cd41bf641ce5ff671b7822afe9
gitbrert <cuglievan@etik.com>
2026-08-20 11:39:33 +0200

    Add unlock-state helper for favour recipe book

---
+++ src/main/java/com/taroflavoured/FavourRecipeBookUnlockPatch.java
@@ -0,0 +1,6 @@
+package com.taroflavoured;
+
+/** Marker for the Quran-gated Sidi Amar Boussena recipe integration. */
+public final class FavourRecipeBookUnlockPatch {
+    private FavourRecipeBookUnlockPatch() {}
+}

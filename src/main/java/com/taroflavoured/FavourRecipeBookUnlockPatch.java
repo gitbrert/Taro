0fa785fdc9c6da1553fa0bdbd251e005c769d916
gitbrert <cuglievan@etik.com>
2026-08-20 11:40:07 +0200

    Remove temporary recipe-book patch marker

--- src/main/java/com/taroflavoured/FavourRecipeBookUnlockPatch.java
+++
@@ -1,6 +0,0 @@
-package com.taroflavoured;
-
-/** Marker for the Quran-gated Sidi Amar Boussena recipe integration. */
-public final class FavourRecipeBookUnlockPatch {
-    private FavourRecipeBookUnlockPatch() {}
-}

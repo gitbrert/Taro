e3181cadcdd5fe71f4c2db8c3710d9341c140fdb
gitbrert <cuglievan@etik.com>
2026-08-20 11:36:06 +0200

    Remove unused recipe holder import

--- src/main/java/com/taroflavoured/FavourRecipeBook.java
+++ src/main/java/com/taroflavoured/FavourRecipeBook.java
@@ -7,7 +7,6 @@ import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.RecipeHolder;
 
 import java.util.ArrayList;
 import java.util.List;

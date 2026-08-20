5665c1ecb0691a91cee7d833174e5ed39554f7fa
gitbrert <cuglievan@etik.com>
2026-08-20 17:09:04 +0200

    Revert custom Favour recipe book type

--- src/main/java/com/taroflavoured/FavourMenu.java
+++ src/main/java/com/taroflavoured/FavourMenu.java
@@ -135,7 +135,7 @@ public class FavourMenu extends RecipeBookMenu<CraftingInput, FavourRecipe> {
     @Override public int getGridWidth() { return RECIPE_GRID_SIZE; }
     @Override public int getGridHeight() { return 1; }
     @Override public int getSize() { return RECIPE_GRID_SIZE; }
-    @Override public RecipeBookType getRecipeBookType() { return TaroFlavoured.FAVOUR_RECIPE_BOOK; }
+    @Override public RecipeBookType getRecipeBookType() { return RecipeBookType.CRAFTING; }
     @Override public List<RecipeBookCategories> getRecipeBookCategories() { return List.of(RecipeBookCategories.CRAFTING_MISC); }
     @Override public boolean shouldMoveToInventory(int slotIndex) { return slotIndex == OUTPUT_SLOT; }
 

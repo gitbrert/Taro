7042bb8d68e396ca0b2cf6b4d40f2bdd21b595a1
gitbrert <cuglievan@etik.com>
2026-08-20 13:48:00 +0200

    Register dedicated Favour recipe-book category

--- src/main/java/com/taroflavoured/FavourClient.java
+++ src/main/java/com/taroflavoured/FavourClient.java
@@ -1,9 +1,13 @@
 package com.taroflavoured;
 
+import net.minecraft.client.RecipeBookCategories;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.client.event.RegisterRecipeBookCategoriesEvent;
+
+import java.util.List;
 
 @EventBusSubscriber(modid = TaroFlavoured.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class FavourClient {
@@ -13,5 +17,11 @@ public final class FavourClient {
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
         event.register(TaroFlavoured.FAVOUR_MENU.get(), FavourScreen::new);
+    }
+
+    @SubscribeEvent
+    public static void registerRecipeBookCategories(RegisterRecipeBookCategoriesEvent event) {
+        event.registerBookCategories(TaroFlavoured.FAVOUR_RECIPE_BOOK, List.of(RecipeBookCategories.CRAFTING_MISC));
+        event.registerRecipeCategoryFinder(TaroFlavoured.FAVOUR_RECIPE_TYPE.get(), recipe -> RecipeBookCategories.CRAFTING_MISC);
     }
 }

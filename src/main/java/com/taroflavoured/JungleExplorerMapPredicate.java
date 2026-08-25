dc51614f8d9661ab258df9e808fd41b65d6e57f4
gitbrert <cuglievan@etik.com>
2026-08-25 18:17:19 +0200

    Add Jungle Explorer Map predicate helper

---
+++ src/main/java/com/taroflavoured/JungleExplorerMapPredicate.java
@@ -0,0 +1,12 @@
+package com.taroflavoured;
+
+import net.minecraft.world.item.ItemStack;
+
+/** Placeholder for the 1.21.1 Jungle Explorer Map matching predicate. */
+public final class JungleExplorerMapPredicate {
+    private JungleExplorerMapPredicate() {}
+
+    public static boolean matches(ItemStack stack) {
+        return false;
+    }
+}
